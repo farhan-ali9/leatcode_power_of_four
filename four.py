e224776c4a90769fda77c7df20b8a94fc1bc8cd7
Farhan Ali <80478326+farhan-ali9@users.noreply.github.com>
2023-07-26 21:17:28 +0500

    Update four.py

--- four.py
+++ four.py
@@ -1,3 +1,14 @@
+#first soltion
+class Solution(object):
+    def isPowerOfFour(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        return n > 0 and n & (n - 1) == 0 and n & 0x55555555 != 0
+
+
+#second solution
 class Solution(object):
     def isPowerOfFour(self, n):
         """
@@ -9,3 +20,20 @@ class Solution(object):
         while n%4==0: #5!=0
             n=n//4
         return n==1
+#third solution 
+class Solution(object):
+    def isPowerOfFour(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n<=0:
+            return False
+        elif n==1:
+            return True
+        elif n%4==0:
+            return True
+        else:
+            return False
+
+                
